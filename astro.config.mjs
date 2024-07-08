2ee02a0383eb0eb01e9ac7730dfd03c8f31afdf8
tabrez <tabrez.akhtar1@gmail.com>
2024-07-08 16:10:24 +0100

    greetings component in preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://ornate-lebkuchen-53cfd4.netlify.app/"
+  site: "https://ornate-lebkuchen-53cfd4.netlify.app/",
+  integrations: [preact()]
 });
