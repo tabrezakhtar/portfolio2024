8df535407f014574b8a448e13212a54ebe4caa08
tabrez <tabrez.akhtar1@gmail.com>
2024-07-11 20:03:17 +0100

    update the title

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -136,9 +136,9 @@
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 100,
+      typeSpeed: 70,
       backSpeed: 50,
-      backDelay: 1500
+      backDelay: 1000
     });
   }
 
